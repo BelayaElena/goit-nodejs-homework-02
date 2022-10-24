d6bc731ca258b3f907feee9a79a383e940ab0efd
ElenaBelaya <makseniya@ukr.net>
2022-10-24 20:49:21 +0300

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-//const { PORT = 3000 } = process.env;
-app.listen(3010, () => {
-  console.log(`Server running`);
-});
+DB_HOST =
+  "mongodb+srv://BelayaElena:SGJYdPuSqmEzvcne@cluster0.hhwzkli.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+const { PORT = 3000 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(app.listen(PORT))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
